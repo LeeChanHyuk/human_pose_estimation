44a4da218c031a320655e8be6c229509ad0b6670
leechanhyuk <cjsgkcksgur@naver.com>
2022-02-18 15:41:40 +0900

    Multi feature training setup is completed

--- human_pose/template/trainer/new_dataset/ch_dataset.py
+++ human_pose/template/trainer/new_dataset/ch_dataset.py
@@ -1,5 +1,6 @@
 # init에서는 dataset을 create하는 함수를 만들어야지.
 import logging
+from aiohttp import BodyPartReader
 from matplotlib.pyplot import axis
 import torch
 import torchvision
@@ -34,18 +35,48 @@ class ch_dataset(torch.utils.data.Dataset):
         for index, action in enumerate(os.listdir(self.data_path)):
             index = int(action[0])
             for pose in os.listdir(os.path.join(self.data_path, action)):
+                """Normalize standardrizaton
+                transition_x : image_width
+                transition_y : image_height
+                head, body poses : 90
+                gaze_poses : 1
+                """
                 poses_from_one_video = np.load(os.path.join(self.data_path, action, pose))
-                normalized_pose = []
-                for i in range(poses_from_one_video.shape[-1]):
-                    i_all = poses_from_one_video[:,i]
+                center_eyes = poses_from_one_video[0, :, :3]
+                center_mouths = poses_from_one_video[1, :, :3]
+                left_shoulders = poses_from_one_video[2, :, :3]
+                right_shoulders = poses_from_one_video[3, :, :3]
+                center_stomachs = poses_from_one_video[4, :, :3]
+
+                # transition normalization
+                for i in range(center_eyes.shape[-1]):
+                    if i == 0:
+                        div_num = 640
+                    elif i == 1:
+                        div_num = 480
+                    else:
+                        div_num = 256
+                    center_eyes[:,i] /= div_num
+                    center_mouths[:,i] /= div_num
+                    left_shoulders[:,i] /= div_num
+                    right_shoulders[:,i] /= div_num
+                    center_stomachs[:,i] /= div_num
+                    
+                head_poses = poses_from_one_video[5, :, :3] / 90
+                body_poses = poses_from_one_video[6, :, :3] / 90
+                gaze_poses = poses_from_one_video[7]
+                all_poses = np.concatenate([center_eyes, center_mouths, left_shoulders, right_shoulders, center_stomachs, head_poses, body_poses, gaze_poses], axis=1)
+                normalized_poses = []
+                for i in range(all_poses.shape[-1]):
+                    i_all = all_poses[:,i]
                     i_1 = i_all[0:len(i_all):3]
                     i_2 = i_all[1:len(i_all):3]
                     i_3 = i_all[2:len(i_all):3]
-                    while len(i_1) < 20:
+                    while len(i_1) < all_poses.shape[0] / 3:
                         i_1 = np.concatenate([i_1, np.expand_dims(np.array(i_1[-1]), axis=0)], axis=0)
-                    while len(i_2) < 20:
+                    while len(i_2) < all_poses.shape[0] / 3:
                         i_2 = np.concatenate([i_2, np.expand_dims(np.array(i_2[-1]), axis=0)], axis=0)
-                    while len(i_3) < 20:
+                    while len(i_3) < all_poses.shape[0] / 3:
                         i_3 = np.concatenate([i_3, np.expand_dims(np.array(i_3[-1]), axis=0)], axis=0)
                     i_all = (i_2 + i_2 + i_3) / 3
                     normalized_pose.append(i_all)
@@ -56,9 +87,14 @@ class ch_dataset(torch.utils.data.Dataset):
 
         return np.array(data), np.array(labels)
 
-    def data_normalization(self, data : np.array):
+    def minmax_normalization(self, data : np.array) -> np.array:
         for i in range(data.shape[-1]):
             data[:,i] = (data[:,i] - min(data[:,i])) / (max(data[:,i] - min(data[:,i])))
+        return data
+
+    def data_normalization(self, data : np.array) -> np.array:
+        for i in range(data.shape[-1]):
+            data[:, i] /= data[0, i]
         return data
                 
 
